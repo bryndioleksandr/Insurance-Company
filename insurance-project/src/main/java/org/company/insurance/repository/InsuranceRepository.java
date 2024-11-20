00758defbe54d0d4613403ea506147f6a0ed480e
bryndioleksandr <s.v.bryndo@gmail.com>
2024-11-21 01:19:05 +0200

    Deleted some useless files. Now 73 endpoints on controllers generally

--- insurance-project/src/main/java/org/company/insurance/repository/InsuranceRepository.java
+++
@@ -1,7 +0,0 @@
-//package org.company.insurance.repository;
-//
-//import org.company.insurance.entity.Insurance;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface InsuranceRepository extends JpaRepository<Insurance, Long> {
-//}
